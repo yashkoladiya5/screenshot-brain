3c6abbde045e8a27945e5315e741bb1bb6f9c3dc
Yash Koladiya <yashkoladiya5@gmail.com>
2026-07-28 10:05:31 +0530

    Add entry point comment

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app.dart';
 import 'services/database_service.dart';
 
+// 1. App entry point
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await DatabaseService.init();
