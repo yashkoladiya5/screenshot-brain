eccac77df22c6246dc21c960a16d9be976bcc71e
Yash Koladiya <yashkoladiya5@gmail.com>
2026-08-13 17:55:36 +0530

    Feat: Add SbShimmer component for shimmering loading effect overlays

---
+++ lib/core/components/sb_shimmer.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class SbShimmer extends StatefulWidget {
+  final Widget child;
+  final Duration duration;
+
+  const SbShimmer({
+    super.key,
+    required this.child,
+    this.duration = const Duration(milliseconds: 1500),
+  });
+
+  @override
+  State<SbShimmer> createState() => _SbShimmerState();
+}
+
+class _SbShimmerState extends State<SbShimmer> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: widget.duration,
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final colorScheme = theme.colorScheme;
+    
+    final baseColor = colorScheme.surfaceContainerHighest.withValues(alpha: 0.2);
+    final highlightColor = colorScheme.surfaceContainerHighest.withValues(alpha: 0.6);
+
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return ShaderMask(
+          blendMode: BlendMode.srcATop,
+          shaderCallback: (bounds) {
+            return LinearGradient(
+              colors: [baseColor, highlightColor, baseColor],
+              stops: const [0.1, 0.3, 0.4],
+              begin: const Alignment(-1.0, -0.3),
+              end: const Alignment(1.0, 0.3),
+              transform: _SlidingGradientTransform(slidePercent: _controller.value),
+            ).createShader(bounds);
+          },
+          child: child,
+        );
+      },
+      child: widget.child,
+    );
+  }
+}
+
+class _SlidingGradientTransform extends GradientTransform {
+  final double slidePercent;
+
+  const _SlidingGradientTransform({required this.slidePercent});
+
+  @override
+  Matrix4? transform(Rect bounds, {TextDirection? textDirection}) {
+    return Matrix4.translationValues(bounds.width * (slidePercent * 2 - 1), 0.0, 0.0);
+  }
+}
